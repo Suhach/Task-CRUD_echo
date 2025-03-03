621b10f3b3e96d042a0d0b0e22e71e3d22217860
Examples <61053710+Suhach@users.noreply.github.com>
2025-03-03 20:12:10 +0300

    Update db.go

--- internal/database/db.go
+++ internal/database/db.go
@@ -7,14 +7,14 @@ import (
 	"gorm.io/gorm"
 )
 
-// ---------переменная----для---использования---DB----\\
+
 var DB *gorm.DB
 
-// ----------------------func--------------------------\\
+
 func InitDB() {
-	dsn := "host=localhost user=postgres password=1234567 dbname=postgres port=5432 sslmode=disable" // dsn
-	var err error                                                                                    // error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})                                          // init DB
+	dsn := "host=localhost user=postgres password=1234567 dbname=postgres port=5432 sslmode=disable" 
+	var err error                                                                                    
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})                                          
 	if err != nil {
 		log.Fatalf("Failed to connect to DataBase: %v", err)
 	}
